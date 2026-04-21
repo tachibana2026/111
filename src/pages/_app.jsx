114e43f2167643d5dfeeac8824b53cfdd459b783
tachibana2026 <yuma720087@gmail.com>
2026-04-21 23:38:35 +0900

    feat: setup google analytics and update seo config

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Script from 'next/script';
 import { motion, AnimatePresence } from 'framer-motion';
 import '../index.css';
 import Layout from '../components/Layout';
 import { Analytics } from '@vercel/analytics/next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import * as gtag from '../lib/gtag';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
@@ -14,7 +16,10 @@ function MyApp({ Component, pageProps }) {
     const handleStart = (url) => {
       if (url !== router.pathname) setLoading(true);
     };
-    const handleComplete = () => setLoading(false);
+    const handleComplete = (url) => {
+      setLoading(false);
+      gtag.pageview(url);
+    };
 
     router.events.on('routeChangeStart', handleStart);
     router.events.on('routeChangeComplete', handleComplete);
@@ -29,6 +34,28 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <>
+      {gtag.GA_TRACKING_ID && (
+        <>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${gtag.GA_TRACKING_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
+        </>
+      )}
       <AnimatePresence>
         {loading && (
           <motion.div
